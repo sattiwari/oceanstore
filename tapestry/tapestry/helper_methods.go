616b45743b57b28acae22bf8cc3e633ffb8708e5
Satendra Tiwari <sattiwari18@gmail.com>
2016-12-08 19:50:39 +0700

    make tapestry node for tests

--- tapestry/tapestry/helper_methods.go
+++ tapestry/tapestry/helper_methods.go
@@ -1,8 +1,24 @@
 package tapestry
 
+import "testing"
+
+var port int
+
 func equal_ids(id1, id2 ID) bool {
 	if SharedPrefixLength(id1, id2) == DIGITS {
 		return true
 	}
 	return false
+}
+
+func makeTapestryNode(id ID, addr string, t *testing.T) *TapestryNode {
+
+	tapestry, err := start(id, port, addr)
+
+	if err != nil {
+		t.Errorf("Error while making a tapestry %v", err)
+	}
+
+	port++
+	return tapestry.local
 }
