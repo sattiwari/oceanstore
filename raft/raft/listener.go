4b53010aa5a20eb34403285c0d55273b4d09bc73
Satendra Tiwari <sattiwari18@gmail.com>
2016-12-17 21:56:43 +0700

    utility function to open port on random or given port number

---
+++ raft/raft/listener.go
@@ -0,0 +1,48 @@
+package raft
+
+import (
+	"net"
+	"math/rand"
+	"time"
+	"os"
+	"fmt"
+	"syscall"
+)
+
+// ephemeral port range
+const LOW_PORT int = 32768
+const HIGH_PORT int = 61000
+
+func OpenListener() (net.Listener, int, error) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	port := rand.Intn(HIGH_PORT - LOW_PORT) + LOW_PORT
+	conn, err := OpenPort(port)
+	if err != nil {
+		if addrInUse(err) {
+			time.Sleep(time.Millisecond * 100)
+			return OpenListener()
+		} else {
+			return err, -1, err
+		}
+	}
+	return conn, port, err
+}
+
+func addrInUse(err error) bool {
+	if opErr, ok := err.(*net.OpError); ok {
+		if osErr, ok := opErr.Err.(os.SyscallError); ok {
+			return osErr.Err == syscall.EADDRINUSE
+		}
+	}
+	return false
+}
+
+func OpenPort(port int) (net.Listener, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+	addr := fmt.Sprintf("%v:%v", hostname, port)
+	conn, err := net.Listen("tcp4", addr)
+	return conn, err
+}
