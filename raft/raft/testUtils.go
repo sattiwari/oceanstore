2af41f16832a5ab7dc70c71972cb8e74bcbd16b6
Satendra Tiwari <sattiwari18@gmail.com>
2017-01-02 09:14:05 +0700

    test related utils

--- raft/raft/testUtils.go
+++ raft/raft/testUtils.go
@@ -53,8 +53,78 @@ func randSeq(n int) string {
 func checkNodes(nodes []*RaftNode, clusterSize int) bool {
 	for _, n := range nodes {
 		if len(n.GetOtherNodes()) != clusterSize {
+			Error.Println(len(n.GetOtherNodes()), clusterSize)
 			return false
 		}
 	}
 	return true
+}
+
+func printNodes(nodes []*RaftNode) {
+	for _, n := range nodes {
+		n.PrintLogCache()
+		n.ShowState()
+	}
+}
+
+func checkMajorityTerms(nodes []*RaftNode) bool {
+	sums := make(map[uint64]int, nodes[0].conf.ClusterSize)
+	for _, n := range nodes {
+		sums[n.GetCurrentTerm()]++
+	}
+	max := -1
+	for _, v := range sums {
+		if v > max {
+			max = v
+		}
+	}
+
+	if max > len(nodes)/2 {
+		return true
+	}
+	return false
+}
+
+func checkMajorityCommitIndex(nodes []*RaftNode) bool {
+	sums := make(map[uint64]int, nodes[0].conf.ClusterSize)
+	for _, n := range nodes {
+		sums[n.commitIndex]++
+	}
+	max := -1
+	for _, v := range sums {
+		if v > max {
+			max = v
+		}
+	}
+
+	if max > len(nodes)/2 {
+		return true
+	}
+	return false
+}
+
+func checkLogOrder(nodes []*RaftNode) bool {
+	for _, n := range nodes {
+		prevIndex := int64(-1)
+		prevTerm := int64(-1)
+		seen := make(map[uint64]bool)
+		for _, entry := range n.logCache {
+			if seen[entry.Index] || int64(entry.Index)-1 != prevIndex || int64(entry.Term) < prevTerm {
+				return false
+			}
+
+			seen[entry.Index] = true
+			prevIndex = int64(entry.Index)
+			prevTerm = int64(entry.Term)
+		}
+	}
+	return true
+}
+
+func shutdownNodes(nodes []*RaftNode) {
+	for _, n := range nodes {
+		n.IsShutDown = true
+		n.gracefulExit <- true
+	}
+	time.Sleep(time.Millisecond * 200)
 }
