e8f2a4dfb9fdaff8a0def521ba7c0b3eb11aa48d
Satendra Tiwari <sattiwari18@gmail.com>
2017-01-02 09:14:40 +0700

    test leader election

--- raft/raft/raft_test.go
+++ raft/raft/raft_test.go
@@ -2,12 +2,80 @@ package raft
 
 import (
 	"testing"
+	//"fmt"
+	"time"
 	"fmt"
 )
 
-func TestNodeClusterCreation(t *testing.T)  {
-	conf := DefaultConfig()
-	nodes, err := CreateCluster(conf)
+func TestLeaderElection(t *testing.T) {
+	config := DefaultConfig()
+	config.ClusterSize = 5
+	config.LogPath = randSeq(10)
+
+	nodes, err := CreateCluster(config)
+	if err != nil {
+		t.Errorf("Could not create nodes")
+		return
+	}
+	time.Sleep(time.Millisecond * 500)
+	if !checkNodes(nodes, config.ClusterSize) {
+		t.Errorf("CreateLocalCluster FAILED")
+		return
+	}
+
+	fmt.Printf("Before loop\n")
+	getLeader(nodes)
 	leader := getLeader(nodes)
-	fmt.Print(leader, err)
+	fmt.Printf("after loop\n")
+	if leader == nil {
+		t.Errorf("Not found the leader")
+		fmt.Printf("# nodes: %v\n", len(nodes))
+		printNodes(nodes)
+		return
+	}
+
+	time.Sleep(time.Millisecond * 500)
+	if !checkMajorityTerms(nodes) {
+		t.Errorf("Nodes are not on the same term (%v)", leader.GetCurrentTerm())
+	}
+	if !checkMajorityCommitIndex(nodes) {
+		t.Errorf("Nodes dont have the same commit index (%v)", leader.commitIndex)
+	}
+	if !checkLogOrder(nodes) {
+		t.Errorf("Nodes logs are not in an ok order")
+		printNodes(nodes)
+	}
+
+	fmt.Printf("The disabled node is: %v\n", leader.Id)
+	leader.Testing.PauseWorld(true)
+	disableLeader := leader
+	time.Sleep(time.Millisecond * 100)
+	leader = getLeader(nodes)
+	if leader == nil {
+		t.Errorf("Leader is not the same %v is not located in node", leader.Id)
+		return
+	}
+
+	fmt.Printf("We now enable %v\n", disableLeader.Id)
+	disableLeader.Testing.PauseWorld(false)
+	time.Sleep(time.Millisecond * 100)
+	leader = getLeader(nodes)
+	if leader == nil {
+		t.Errorf("Leader is not the same %v is not located in node", leader.Id)
+		return
+	}
+	time.Sleep(time.Millisecond * 500)
+	if !checkMajorityTerms(nodes) {
+		t.Errorf("Nodes are not on the same term (%v)", leader.GetCurrentTerm())
+	}
+	if !checkMajorityCommitIndex(nodes) {
+		t.Errorf("Nodes dont have the same commit index (%v)", leader.commitIndex)
+	}
+	if !checkLogOrder(nodes) {
+		t.Errorf("Nodes logs are not in an ok order")
+		printNodes(nodes)
+	}
+
+	fmt.Println("TestLeaderElection pass")
+	shutdownNodes(nodes)
 }
