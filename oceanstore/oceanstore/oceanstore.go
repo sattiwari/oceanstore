51ace252dc7f27634006222ff5639a308eec281a
Satendra Tiwari <sattiwari18@gmail.com>
2017-01-04 12:19:35 +0700

    defined ocean node

--- oceanstore/oceanstore/oceanstore.go
+++ oceanstore/oceanstore/oceanstore.go
@@ -1,5 +1,22 @@
 package oceanstore
 
+import (
+	"../../raft/raft"
+	"../../tapestry/tapestry"
+)
+
 type OceanAddr struct {
 	Addr string
+}
+
+type OceanNode struct {
+	tnodes      []*tapestry.Tapestry
+	rnodes      []*raft.RaftNode
+	rootV       uint32
+	clientPaths map[uint64]string       // client id -> curpath
+	clients     map[uint64]*raft.Client // client id -> client
+
+	Local      OceanAddr
+	raftClient *raft.Client
+	server     *PuddleRPCServer
 }
