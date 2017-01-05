d0eb10e69260ec924217971fab3556e789aed65a
Satendra Tiwari <sattiwari18@gmail.com>
2017-01-05 13:17:48 +0700

    file system RPC server

--- oceanstore/oceanstore/ocean_rpc_impl.go
+++ oceanstore/oceanstore/ocean_rpc_impl.go
@@ -3,10 +3,37 @@ package oceanstore
 import (
 	"net"
 	"net/rpc"
+	"fmt"
 )
 
 type OceanRPCServer struct {
 	node     *OceanNode
 	listener net.Listener
 	rpc      *rpc.Server
+}
+
+func newOceanstoreRPCServer(ocean *OceanNode) (server *OceanRPCServer) {
+	server = new(OceanRPCServer)
+	server.node = ocean
+	server.rpc = rpc.NewServer()
+	listener, _, err := OpenListener()
+	server.rpc.RegisterName(listener.Addr().String(), server)
+	server.listener = listener
+
+	if err != nil {
+		panic("AA")
+	}
+
+	go func() {
+		for {
+			conn, err := server.listener.Accept()
+			if err != nil {
+				fmt.Printf("(%v) Raft RPC server accept error: %v\n", err)
+				continue
+			}
+			go server.rpc.ServeConn(conn)
+		}
+	}()
+
+	return
 }
