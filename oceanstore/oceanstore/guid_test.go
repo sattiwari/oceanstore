14d3887029c1ddeb4631847d5d4b66f835f87998
Satendra Tiwari <sattiwari18@gmail.com>
2017-01-31 23:09:34 +0700

    guid test

--- oceanstore/oceanstore/guid_test.go
+++ oceanstore/oceanstore/guid_test.go
@@ -2,12 +2,66 @@ package oceanstore
 
 import (
 	"testing"
+	"strings"
+	"time"
+	"fmt"
 )
 
 func TestRaftMap(t *testing.T) {
-	_, err := Start()
+	ocean, err := Start()
 	if err != nil {
 		t.Errorf("Could not init puddlestore: %v", err)
 		return
+	}
+	time.Sleep(time.Millisecond * 1000)
+
+	fmt.Println(ocean.Local)
+	client := ocean.raftClient
+
+	err = ocean.setRaftVguid("DEAD", "BEEF", client.Id)
+	if err != nil {
+		t.Errorf("Could set raft vguid: %v", err)
+		return
+	}
+
+	response, err := ocean.getRaftVguid("DEAD", client.Id)
+	if err != nil {
+		t.Errorf("Could get raft vguid: %v", err)
+		return
+	}
+
+	ok := strings.Split(string(response), ":")[0]
+	aguid := strings.Split(string(response), ":")[1]
+
+	if ok != "SUCCESS" {
+		t.Errorf("Could not get raft vguid: %v", response)
+	}
+
+	if aguid != "BEEF" {
+		t.Errorf("Raft didn't return the correct vguid. BEEF != %d", aguid)
+	}
+
+	// Reset aguid to another vguid
+	err = ocean.setRaftVguid("DEAD", "B004", client.Id)
+	if err != nil {
+		t.Errorf("Could set raft vguid: %v", err)
+		return
+	}
+
+	response, err = ocean.getRaftVguid("DEAD", client.Id)
+	if err != nil {
+		t.Errorf("Could get raft vguid: %v", err)
+		return
+	}
+
+	ok = strings.Split(string(response), ":")[0]
+	aguid = strings.Split(string(response), ":")[1]
+
+	if ok != "SUCCESS" {
+		t.Errorf("Could not get raft vguid: %v", response)
+	}
+
+	if aguid != "B004" {
+		t.Errorf("Raft didn't return the correct vguid. B004 != %d", aguid)
 	}
 }
